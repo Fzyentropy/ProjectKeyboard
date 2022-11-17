dd64048c754b7d55e5d54ae86faef8040aea679a
BlaerZhang <zhangyueyang0022@126.com>
2022-11-16 21:30:10 -0500

    Add new soccer mode, in soccer scene

---
+++ Assets/Script/SoccerScore.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using TMPro;
+
+public class SoccerScore : MonoBehaviour
+{
+    private float SScore = 0;
+    public TextMeshProUGUI ScoreText; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        SScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = SScore.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ball")
+        {
+            SScore += 1f;
+            StartCoroutine(KickOff());
+        }
+    }
+
+    IEnumerator KickOff()
+    {
+        yield return new WaitForSeconds(2);
+
+        GameObject.FindWithTag("Ball").transform.position = new Vector3(7.5f, 3.5f, -3.5f);
+
+        // yield return null;
+    }
+}
